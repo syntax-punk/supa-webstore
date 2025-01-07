36d358f636dc9d4c9ed4eecf185ddf74b6fdc49b
syntax-punk <david.jaeren@gmail.com>
2025-01-07 10:02:34 +0100

    Created Header component and working on the main layout

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/native/nav/parent"
+
 function App() {
 
   return (
-    <div>Hello</div>
+    <>
+      <Header />
+      <div className="px-[1.4rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem] 2xl:px-[12rem]">
+        <h1 className="text-3xl font-bold">App</h1>
+      </div>
+    </>
   )
 }
 
