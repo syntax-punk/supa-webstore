fc656f71f529dbfe5fed8d5f794aecc326487983
syntax-punk <david.jaeren@gmail.com>
2025-01-12 23:33:24 +0100

    Product details page wip

--- client/src/features/products/ProductDetails.tsx
+++ client/src/features/products/ProductDetails.tsx
@@ -1,3 +1,11 @@
+import { Product } from "@/app/models/product";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function ProductDetails() {
+
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product>();
+
   return <div>ProductDetails</div>;
 }
